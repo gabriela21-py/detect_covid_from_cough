1f07de6016ea9e48804ef48fee32e8121d370345
gabriela21-py <morarugabrieladiana2003@gmail.com>
2026-02-28 20:14:13 +0200

    delete unused files

--- src/config.py
+++ src/config.py
@@ -4,6 +4,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
 RAW_DIR = PROJECT_ROOT / "data" / "raw"
 COSWARA_DIR = RAW_DIR / "coswara"
+COUGH_DIR = COSWARA_DIR / "cough_extracted"
 
 PROCESSED_DIR = PROJECT_ROOT / "data" / "processed"
 MANIFEST_CSV = PROCESSED_DIR / "manifest.csv"
